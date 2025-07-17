29a94fe90c10b1a1b9fbf672a78f86efbd156c34
CileenAntari <165431073+CileenAntari@users.noreply.github.com>
2025-07-17 14:20:53 +0300

    Program.cs

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,4 +1,4 @@
-﻿using App;
+using App;
 using Contracts;
 using Domain;
 
@@ -7,53 +7,42 @@ internal class Program
     private static void Main(string[] args)
     {
         FoodServices fl = new FoodServices();  
-
         FoodDTO fd1 = new FoodDTO("pizza", "italian", 50);
         FoodDTO fd2 = new FoodDTO("burger", "junk food", 35);
         FoodDTO fd3 = new FoodDTO("pasta", "italian", 45);
-        fl.add(fd1);
-        fl.add(fd2);
-        fl.add(fd3);
-
+        fl.Add(fd1);
+        fl.Add(fd2);
+        fl.Add(fd3);
         var foods = fl.GetAll();
         foreach (var n in foods)
         {
             Console.Write($"{n.Id}:{n.Name} ,");
         }
-
         Console.WriteLine();
-        fl.remove(fd1);
-
+        fl.Remove(fd1.Id);
         var foo = fl.GetAll();
         foreach (var n in foo)
         {
             Console.Write($"{n.Id}:{n.Name} ,");
         }
-
         Console.WriteLine();
-
         FoodDTO fd4 = new FoodDTO("sandwich", "junk food", 35);
         FoodDTO fd5 = new FoodDTO("mansaf", "arabic food", 45);
-        fl.add(fd4);
-        fl.add(fd5);
-
+        fl.Add(fd4);
+        fl.Add(fd5);
         var fo = fl.GetAll();
         foreach (var n in fo)
         {
             Console.Write($"{n.Id}:{n.Name} ,");
         }
-
         Console.WriteLine();
-
         FoodDTO fd6 = new FoodDTO("pesto pasta", "italian", 45);
-        fl.update(fd6, fd3);  
-
+        fl.Update(fd3.Id, fd6);
         var f = fl.GetAll();
         foreach (var n in f)
         {
             Console.Write($"{n.Id}:{n.Name} ,");
         }
-
         Console.ReadKey();
     }
 }
