98207ba9186fa79c424bcf969dc303fb222ab969
CileenAntari <165431073+CileenAntari@users.noreply.github.com>
2025-07-17 14:17:52 +0300

    FoodDTO.cs

--- Contracts/FoodDTO.cs
+++ Contracts/FoodDTO.cs
@@ -1,17 +1,13 @@
-﻿namespace Contracts
+namespace Contracts
 {
     public class FoodDTO
     {
         public string Name { get; set; }
-
         public int Id { get; set; }
-
         public static int count = 1;
-
         public double Price { get; set; }
-
         public string Category { get; set; }
-
+        public FoodDTO() { }
         public FoodDTO(string Name, string Category, double Price)
         {
             this.Id = count++;
@@ -19,7 +15,5 @@
             this.Category = Category;
             this.Price = Price;
         }
-
-        public FoodDTO() { }
     }
 }
